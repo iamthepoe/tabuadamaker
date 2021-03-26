fb06d09c1225993294dfbc49b6c6879f844bc059
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2021-03-26 19:08:46 -0300

    Add files via upload

---
+++ tabuadamaker.py
@@ -0,0 +1,18 @@
+import os
+import time
+res = 'S'
+while(res == 'S') | (res == 's'):
+    print('===========================')
+    print('TABUADA GENERATOR')
+    print('===========================')
+    cont = 1
+    num = int(input('Digite o número desejado: '))
+    tabnum = int(input('Digite quantas vezes ele irá ser multiplicado: '))
+
+    while(cont<=tabnum):
+        print(f'{num} x {cont} = {num * cont}')
+        cont = cont + 1
+
+    print('Deseja escolher novamente? [S/N]')
+    res = str(input(''))
+    os.system('cls') or None
